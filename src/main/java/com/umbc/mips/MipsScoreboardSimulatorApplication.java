bface6e7d894dde5dd3916d88f6cdcaa44bfe87a
Deviprasad Mohapatra <demohapa@gmail.com>
2021-04-16 20:53:50 -0400

    Initial springboot backend created

---
+++ src/main/java/com/umbc/mips/MipsScoreboardSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.umbc.mips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MipsScoreboardSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MipsScoreboardSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umbc/mips/MipsScoreboardSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umbc.mips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MipsScoreboardSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
