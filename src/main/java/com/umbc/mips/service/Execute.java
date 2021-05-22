f87ce52f53d16898fa0a58b25bf2f8a598db0585
Om Rohith Karamala <rohith@Oms-MacBook-Pro.local>
2021-05-21 22:51:01 -0400

    modified execute

--- src/main/java/com/umbc/mips/service/Execute.java
+++ src/main/java/com/umbc/mips/service/Execute.java
@@ -1,93 +1,185 @@
 package com.umbc.mips.service;
-// TODO:
-// [ ] Implement BEQ inst
-// [ ] Implement LA/LD inst
-// [ ] Implement NEGATE
-
 
 class Execute {
-    public static int [] registers = new int[32];
-    private static int [] spec = new int[4];
+    private static int [] registers = new int[32];
 
     public static void execute() {
         // Loop on all instructions
         for(int a = 0; a < Data.instructions.length; a++) {
-            // TODO: iterate over all the instructions and based on the
-            // kind of the instruction execute them;
+            int dest = -1;
             switch(Data.instructions[a][0]) {
-                case "LI":
-                    System.out.println("Instruction LI");
-                    LI(Data.instructions[a]);
-                    printAllRegisters();
-                    break;
-                case "LA":
-                    System.out.println("Instruction LA");
-                    LA(Data.instructions[a]);
-                    printAllRegisters();
-                    break;
-                case "ADDI":
-                    System.out.println("Instruction ADDI");
-                    // TODO: implement ADDI
-                    // ADD(Data.instructions[a]);
-                    break;
-                case "MUL.D":
-                    System.out.println("Instruction MUL.D");
-                    MUL(Data.instructions[a]);
-                    printAllRegisters();
-                    break;
-                case "S.D":
-                    System.out.println("Instruction S.D");
-                    SD(Data.instructions[a]);
-                    printAllRegisters();
-                    break;
-                case "DIV.D":
-                    System.out.println("Instruction DIV.D");
-                    DIV(Data.instructions[a]);
-                    printAllRegisters();
-                    break;
                 case "ADD":
-                    System.out.println("Instruction ADD");
+                    // ADD from registers
                     ADD(Data.instructions[a]);
                     break;
-                case "SW":
-                    System.out.println("Instruction SW");
-                    // TODO: implement SW
-                    // SW(Data.instructions[a]);
-                    break;
-                case "DSUBI":
-                    System.out.println("Instruction DSUBI");
-                    // TODO: implement DSUBI
-                    // DSUBI(Data.instructions[a]);
-                    break;
-                case "BEQ":
-                    System.out.println("Instruction BEQ");
-                    // TODO: implement BEQ
-                    // BEQ(Data.instructions[a]);
-                    break;
-                case "HLT":
-                    System.out.println("Instruction HLT");
-                    // TODO: see if HLT needs to be implemented or left out ?
-                    break;
-                case "MOVE":
-                    System.out.println("Instruction MOVE");
-                    MOVE(Data.instructions[a]);
-                    printAllRegisters();
+                case "ADDI":
+                    // ADD Immediate to register
+                    ADDI(Data.instructions[a]);
                     break;
                 case "AND":
-                    System.out.println("Instruction AND");
+                    // Logical AND from registers
                     AND(Data.instructions[a]);
                     break;
-                case "OR":
-                    System.out.println("Instruction OR");
-                    OR(Data.instructions[a]);
+                case "ANDI":
+                    // Logical AND Immediate
+                    ANDI(Data.instructions[a]);
                     break;
-                case "XOR":
-                    System.out.println("Instruction XOR");
-                    XOR(Data.instructions[a]);
+                case "B":
+                    // Branch instruction
+                    dest = B(Data.instructions[a]);
+                    if(dest == -1) {
+                        System.out.println("Flag not found in instructions");
+                    } else {
+                        a = dest;
+                    }
+                    break;
+                case "BEQ":
+                    // Branch if given registers are equal
+                    dest = BEQ(Data.instructions[a]);
+                    if(dest == -1) {
+                        System.out.println("Flag not found in instructions");
+                    } else {
+                        a = dest;
+                    }
+                    break;
+                case "BEQZ":
+                    // Branch if given register is equal to zero
+                    dest = BEQZ(Data.instructions[a]);
+                    if(dest == -1) {
+                        System.out.println("Flag not found in instructions");
+                    } else {
+                        a = dest;
+                    }
+                    break;
+                case "BGEZ":
+                    // Branch if given register is greater than or equal to zero
+                    dest = BGEZ(Data.instructions[a]);
+                    if(dest == -1) {
+                        System.out.println("Flag not found in instructions");
+                    } else {
+                        a = dest;
+                    }
+                    break;
+                case "BGTZ":
+                    // Branch if given register is greater than 0
+                    dest = BGTZ(Data.instructions[a]);
+                    if(dest == -1) {
+                        System.out.println("Flag not found in instructions");
+                    } else {
+                        a = dest;
+                    }
+                    break;
+                case "BLEZ":
+                    // Branch if given register is less than or equal to 0
+                    dest = BLEZ(Data.instructions[a]);
+                    if(dest == -1) {
+                        System.out.println("Flag not found in instructions");
+                    } else {
+                        a = dest;
+                    }
+                    break;
+                case "BLTZ":
+                    // Branch if given register is less than 0
+                    dest = BLTZ(Data.instructions[a]);
+                    if(dest == -1) {
+                        System.out.println("Flag not found in instructions");
+                    } else {
+                        a = dest;
+                    }
+                    break;
+                case "BNE":
+                    // Branch if given registers are not equal
+                    dest = BNE(Data.instructions[a]);
+                    if(dest == -1) {
+                        System.out.println("Flag not found in instructions");
+                    } else {
+                        a = dest;
+                    }
+                    break;
+                case "BNEZ":
+                    // Branch if given register is not equal to zero
+                    dest = BNEZ(Data.instructions[a]);
+                    if(dest == -1) {
+                        System.out.println("Flag not found in instructions");
+                    } else {
+                        a = dest;
+                    }
+                    break;
+                case "DIV":
+                    // DIV register by register
+                    DIV(Data.instructions[a]);
+                    break;
+                case "LA":
+                    // Load from address
+                    LA(Data.instructions[a]);
+                    break;
+                case "LI":
+                    // Load immediate
+                    LI(Data.instructions[a]);
+                    break;
+                case "MOVE":
+                    // copy from register to register
+                    MOVE(Data.instructions[a]);
                     break;
                 case "MOVN":
-                    System.out.println("Instruction MOVN");
+                    // Move if second source register is not equal to zero
                     MOVN(Data.instructions[a]);
+                    break;
+                case "MOVZ":
+                    // Move if second source register is equal to zero
+                    MOVZ(Data.instructions[a]);
+                    break;
+                case "MUL":
+                    // Multiply registers
+                    MUL(Data.instructions[a]);
+                    break;
+                case "NEG":
+                    // Negate and copy from source to destination
+                    NEG(Data.instructions[a]);
+                    break;
+                case "NOP":
+                    // Do nothing
+                    NOP();
+                    break;
+                case "NOR":
+                    // Bitwise NOR
+                    NOR(Data.instructions[a]);
+                    break;
+                case "NOT":
+                    // Bitwise NOT
+                    NOT(Data.instructions[a]);
+                    break;
+                case "OR":
+                    // Bitwise OR
+                    OR(Data.instructions[a]);
+                    break;
+                case "ORI":
+                    // Bitwise OR with Immediate
+                    ORI(Data.instructions[a]);
+                    break;
+                case "SD":
+                    // Store from register into memory
+                    SD(Data.instructions[a]);
+                    break;
+                case "SLT":
+                    // Set destination register to 1 if reg1 < reg2
+                    SLT(Data.instructions[a]);
+                    break;
+                case "SLTI":
+                    // Set destination register to 1 if given src1 < src2
+                    SLTI(Data.instructions[a]);
+                    break;
+                case "SUB":
+                    // Arithmetic subtraction
+                    SUB(Data.instructions[a]);
+                    break;
+                case "XOR":
+                    // Bitwise XOR
+                    XOR(Data.instructions[a]);
+                    break;
+                case "XORI":
+                    // Bitwise XORI
+                    XORI(Data.instructions[a]);
                     break;
                 default:
                     System.out.println("Instruction Not Supported");
@@ -106,7 +198,7 @@ class Execute {
         return reg_val + offset;
     }
 
-    private static void NEGU(String []inst) {
+    private static void NEG(String []inst) {
         int destination = findReg(inst[1]);
         int source = findReg(inst[2]);
 
@@ -168,7 +260,7 @@ class Execute {
             return ;
         }
 
-        registers[destination] = registers[source1] / registers[source2];
+        registers[destination] = reg1 / reg2;
     }
 
     private static void AND(String []inst) {
@@ -250,7 +342,7 @@ class Execute {
         int source2 = findReg(inst[3]);
 
         if(source2 == 0) {
-            registers[destination] = source2;
+            registers[destination] = registers[source1];
         }
     }
 
@@ -292,18 +384,6 @@ class Execute {
         registers[reg] = val;
     }
 
-    private static void LD(String []inst) {
-        // Load from memory
-        int reg = findReg(inst[1]);
-        int add = findAddress(inst[2]);
-
-        try {
-            registers[reg] = Memory.get(add);
-        } catch(Exception e) {
-            System.out.println(e);
-        }
-    }
-
     private static void SD(String []inst) {
         // Store data into memory
         int reg = findReg(inst[1]);
@@ -322,6 +402,110 @@ class Execute {
         for(int a = 0; a < registers.length; a++) {
             System.out.print("R" + a + ": " + registers[a] + " ");
         }
+    }
+
+    private static int BEQ(String []inst) {
+        int reg1 = findReg(inst[1]);
+        int reg2 = findReg(inst[2]);
+        String flag = inst[3];
+
+        if(registers[reg1] == registers[reg2]) {
+            return Data.flags.get(flag);
+        }
+
+        return -1;
+    }
+
+    private static int BEQZ(String []inst) {
+        int reg1 = findReg(inst[1]);
+        String flag = inst[2];
+
+        if(registers[reg1] == 0) {
+            return Data.flags.get(flag);
+        }
+
+        return -1;
+    }
+
+    private static int BGEZ(String []inst) {
+        int reg = findReg(inst[1]);
+        String flag = inst[2];
+
+        if(registers[reg] >= 0) {
+            return Data.flags.get(flag);
+        }
+
+        return -1;
+    }
+
+    private static int BGTZ(String []inst) {
+        int reg = findReg(inst[1]);
+        String flag = inst[2];
+
+        if(registers[reg] > 0) {
+            return Data.flags.get(flag);
+        }
+
+        return -1;
+    }
+
+    private static int BLEZ(String []inst) {
+        int reg = findReg(inst[1]);
+        String flag = inst[2];
+
+        if(registers[reg] <= 0) {
+            return Data.flags.get(flag);
+        }
+
+        return -1;
+    }
+
+    private static int BLTZ(String []inst) {
+        int reg = findReg(inst[1]);
+        String flag = inst[2];
+
+        if(registers[reg] < 0) {
+            return Data.flags.get(flag);
+        }
+
+        return -1;
+    }
+
+    private static int B(String []inst) {
+        String flag = inst[1];
+
+        if(Data.flags.containsKey(flag)) {
+            return Data.flags.get(flag);
+        }
+
+        return -1;
+    }
+
+    private static int BNE(String []inst) {
+        int reg1 = findReg(inst[1]);
+        int reg2 = findReg(inst[2]);
+        String flag = inst[3];
+
+        if(registers[reg1] != registers[reg2]) {
+            return Data.flags.get(flag);
+        }
+
+        return -1;
+    }
+
+    private static int BNEZ(String []inst) {
+        int reg = findReg(inst[1]);
+        String flag = inst[2];
+
+        if(registers[reg] != 0) {
+            return Data.flags.get(flag);
+        }
+
+        return -1;
+    }
+
+    private static void NOP() {
+        // DO NOTHING
     }
 
     private static int findReg(String val) {
